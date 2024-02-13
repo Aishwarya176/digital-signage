86f2a5e9c092797eea05c1b0940bd382c679a672
aishwarya <aishwarya.k@exceloid.com>
2024-02-13 14:09:40 +0530

    latest-added

--- pages/api/getPlaylistData.js
+++ pages/api/getPlaylistData.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
             url: `https://dev-api.cwcloud.in/digitalsignage/playlist/${playlistId}`,
             headers: {
                 "Content-Type": "application/json",
-                "auth_token": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjdyIsInJvbGUiOiJBOEY3RkI3MDI2QkI0MUNBQjdEOEZDMTM1MDI2NjBEMyIsImV4cCI6MTcwNzgwNTU3NSwiaWF0IjoxNzA3NzE5MTc1fQ.XzQfRqY3hgUCTK2L5KB-uZiEJjNs8cTy5fxInFz7cEBvi5Uc8iRwSCnL9FyEHhE8c6OPVmYq4w9mnJR1aLlDYQ"
+                "auth_token": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjdyIsInJvbGUiOiJBOEY3RkI3MDI2QkI0MUNBQjdEOEZDMTM1MDI2NjBEMyIsImV4cCI6MTcwNzg5MzQwNywiaWF0IjoxNzA3ODA3MDA3fQ.TavUMn_pNswJjIDF4cfMFZ0uk2sfI_V59f3hrUx-oQpJtJVkk645Hq7qPb9YCxLcscDWX6KPY8TXcjqDJriaxA"
             }
         });
         const menuList = response.data;
